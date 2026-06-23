8d69598df1473298bef9e0a8a3ec7222ac8e6d0e
DevolTime <yefriathertua@gmail.com>
2026-06-22 23:53:32 -0500

    Creacion del header y footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: Home },
+    { path: 'login', loadComponent: () => import('./features/login/login').then(m => m.Login) },
+    { path: 'register', loadComponent: () => import('./features/register/register').then(m => m.Register) },
+    { path: 'checkout', loadComponent: () => import('./features/checkout/checkout').then(m => m.Checkout) },
+    { path: 'menu', loadComponent: () => import('./features/menu/menu').then(m => m.Menu) },
+    { path: 'categoria', loadComponent: () => import('./features/categoria/categoria').then(m => m.Categoria) },
+    { path: 'contacto', loadComponent: () => import('./features/contacto/contacto').then(m => m.Contacto) },
+    { path: '404', loadComponent: () => import('./features/page-not-found/page-not-found')},
+    { path: 'products-details', loadComponent: () => import('./features/products-details/products-details').then(m => m.ProductsDetails) },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: '**', redirectTo: '404', pathMatch: 'full' }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal, inject } from '@angular/core';
+import { Header } from './shared/components/header/header';
+import { Footer } from './shared/components/footer/footer';
+import { RouterOutlet, Router } from '@angular/router'; // Importamos Router
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, Footer, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('Fronted');
+  protected router = inject(Router); //  Inyectamos el Router
 }
