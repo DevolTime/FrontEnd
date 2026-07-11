7f7224b815af07ff0e5a1887f85bd5ecf72f2360
DevolTime <yefriathertua@gmail.com>
2026-07-11 18:24:26 -0500

    Form category

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,7 @@ import { Component, signal, inject } from '@angular/core';
 import { Header } from './shared/components/header/header';
 import { Footer } from './shared/components/footer/footer';
 import { RouterOutlet, Router } from '@angular/router'; // Importamos Router
+import { CategoryNewForm } from './features/category/category-new-form/category-new-form';
 
 @Component({
   selector: 'app-root',
