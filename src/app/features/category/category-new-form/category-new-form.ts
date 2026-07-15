f181b49a5c463c6163fafb4b6a4801679573454a
DevolTime <yefriathertua@gmail.com>
2026-07-14 20:23:55 -0500

    a

--- src/app/features/category/category-new-form/category-new-form.ts
+++ src/app/features/category/category-new-form/category-new-form.ts
@@ -16,11 +16,30 @@ export class CategoryNewForm {
 
   categoryId: string | null = null;
 
+  viewMode: 'form' | 'list' = 'form'; 
+  categories: any[] = [];
+
   constructor() {
     this.formData = new FormGroup({
       name: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]),
       image: new FormControl(''),
       status: new FormControl('', [Validators.required]),
+    });
+  }
+
+  showCreate() {
+    this.viewMode = 'form';
+    this.formData.reset();
+    this.categoryId = null;
+  }
+
+  showList() {
+    this.viewMode = 'list';
+    this.httpCategory.getCategories().subscribe({
+      next: (data: any) => {
+        this.categories = data;
+      },
+      error: (err) => console.error('Error al listar', err)
     });
   }
 
