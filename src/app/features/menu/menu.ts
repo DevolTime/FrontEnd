bc564b98311770172c34109cb8afc705c8da2076
DevolTime <yefriathertua@gmail.com>
2026-07-27 19:30:09 -0500

    ajuste de carrito, aparece en menu y home

--- src/app/features/menu/menu.ts
+++ src/app/features/menu/menu.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CartFloating } from '../../shared/components/cart-floating/cart-floating';
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  imports: [CartFloating],
   templateUrl: './menu.html',
   styleUrl: './menu.css',
 })
