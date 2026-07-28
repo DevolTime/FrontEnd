1994fc426e8c1841387547f8be09e0a3bd04bcc8
andres rodriguez <roandres730@gmail.com>
2026-07-27 19:14:30 -0500

    hola

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-export const environment = {};
-=======
+
 export const environment = {
     production: true,  // Esta en produccion (Subido en nuestro servidor)
     apiUrl: '', // (BackEnd) URL de servidor remoto (AWS, Google Cloud, Azure, Digital Ocean, etc)
 };
->>>>>>> c4ede166e65bbf2abd8e829db1f257774fb5d45f
