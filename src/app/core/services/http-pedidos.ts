d6e3f7c9560bfd04b5ca2f077b919c87022a9e55
andres rodriguez <roandres730@gmail.com>
2026-08-18 18:41:59 -0500

    fusion

--- src/app/core/services/http-pedidos.ts
+++ src/app/core/services/http-pedidos.ts
@@ -1,12 +1,38 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Service } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 
-@Service()
+@Injectable({
+  providedIn: 'root'
+})
 export class HttpPedidos {
-    private http = inject (HttpClient);
 
-    newPedidos (Pedidos: any) {
-        return this.http.post ('http://localhost:3000/api/pedidos', Pedidos);
-    }
+  private http = inject(HttpClient);
+
+  newPedidos(pedido: any) {
+    return this.http.post(
+      'http://localhost:3000/api/pedidos',
+      pedido
+    );
+  }
+
+  getPedidos() {
+    return this.http.get(
+      'http://localhost:3000/api/pedidos'
+    );
+  }
+
+  deletePedidos(id: string) {
+    return this.http.delete(
+      `http://localhost:3000/api/pedidos/${id}`
+    );
+  }
+
+  updatePedidos(id: string, pedido: any) {
+    return this.http.patch(
+      `http://localhost:3000/api/pedidos/${id}`,
+      pedido
+    );
+  }
+
 }
 
