bf9c08302f645421c5fa9124f0348d872236b15c
andres rodriguez <roandres730@gmail.com>
2026-08-20 19:41:47 -0500

    holaa

--- src/app/core/services/http-pedidos.ts
+++ src/app/core/services/http-pedidos.ts
@@ -1,12 +1,9 @@
 import { HttpClient } from '@angular/common/http';
-
 import { inject, Service } from '@angular/core';
-import { environment } from '../../../environments/environment.development';
-
+import { environment } from '../../../environments/environment';
 
 @Service({
-
-})
+  })
 export class HttpPedidos {
 
   private http = inject(HttpClient);
