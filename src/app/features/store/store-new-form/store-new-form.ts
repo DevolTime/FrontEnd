a59c8716dee175ad6abd550ee9ddbb9497bba7b1
kevindresbar <kevindresbar@gmail.com>
2026-09-08 16:56:23 -0500

    stores component

---
+++ src/app/features/store/store-new-form/store-new-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-store-new-form',
+  imports: [],
+  templateUrl: './store-new-form.html',
+  styleUrl: './store-new-form.css',
+})
+export class StoreNewForm {}
